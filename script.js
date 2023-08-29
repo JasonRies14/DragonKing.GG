95ecb02985fae84dc5b7744a762846c7b2dfbf7e
Jason <88320728+JasonRies14@users.noreply.github.com>
2023-08-29 10:17:08 -0700

    first commit
    lolw

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.height = "200px";
+    document.getElementById("main").style.marginTop = "200px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.height = "0";
+    document.getElementById("main").style.marginTop= "0";
+  }
